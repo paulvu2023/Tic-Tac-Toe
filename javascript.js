5f3178fc78c1355f60414a38ff65e04b94d14851
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-08-07 22:55:13 -0700

    Add changeBoard and logBoard functions

--- javascript.js
+++ javascript.js
@@ -1,5 +1,18 @@
-const gameboard = (()=> {
+const gameboard = (() => {
     let board = new Array(9);
     
+    const logBoard = () => {console.log(board)};
+    
+    const changeBoard = (symbol, boardIndex) => {
+        board[boardIndex] = symbol;
+    };
 
+    return {logBoard, changeBoard};
 })();
+
+const playerFactory = (symbol) => {
+    return {symbol};
+}
+
+gameboard.changeBoard('X', 8);
+gameboard.logBoard();
